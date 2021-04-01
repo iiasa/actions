60099b7a64bbdea868d4e588c8fd3b63342a468f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-04-01 17:14:56 +0200

    Add an output value for use in caching

--- setup-gams/script.sh
+++ setup-gams/script.sh
@@ -10,25 +10,35 @@ BASE=$(realpath gams)
 # GAMS source URL fragment, and path fragment for extracted files
 case $RUNNER_OS in
   Linux)
+    CACHE_PATH="$GITHUB_ACTION_PATH/gams.exe"
     GAMS_OS=linux
     FRAGMENT=${GAMS_OS}_x64_64_sfx
     ;;
   macOS)
+    CACHE_PATH="$GITHUB_ACTION_PATH/gams.exe"
     GAMS_OS=macosx
     FRAGMENT=osx_x64_64_sfx
     ;;
   Windows)
+    CACHE_PATH="$GHA_PATH\\gams.exe"
     GAMS_OS=windows
     FRAGMENT=${GAMS_OS}_x64_64
     ;;
 esac
 
+# Path fragment for extraction or install
+DEST=gams$(echo $GAMS_VERSION | cut -d. -f1-2)_$FRAGMENT
+
+# Write to special GitHub Actions environment variable to update $PATH for
+# subsequent workflow steps
+echo "$GITHUB_ACTION_PATH/$DEST" >> $GITHUB_PATH
+echo "::set-output name=cache-path::$CACHE_PATH"
+
 # Retrieve
 BASE_URL=https://d37drm4t2jghv5.cloudfront.net/distributions
 URL=$BASE_URL/$GAMS_VERSION/$GAMS_OS/$FRAGMENT.exe
 
-# NB this currently has no effect, because caching is not configured for this
-#    action
+# curl --time-cond only works if the named file exists
 if [ -x gams.exe ]; then
   # Don't retrieve if the remote file is older than the cached one
   TIME_CONDITION=--remote-time --time-cond gams.exe
@@ -37,9 +47,6 @@ fi
 curl --silent $URL --output gams.exe $TIME_CONDITION
 
 # TODO confirm checksum
-
-# Path fragment for extraction or install
-DEST=gams$(echo $GAMS_VERSION | cut -d. -f1-2)_$FRAGMENT
 
 if [ $GAMS_OS = "windows" ]; then
   # Write a PowerShell script. Install to the same directory as *nix unzip
@@ -54,10 +61,6 @@ else
   # Extract files
   unzip -q gams.exe
 fi
-
-# Write to special GitHub Actions environment variable to update $PATH for
-# subsequent workflow steps
-echo "$GITHUB_ACTION_PATH/$DEST" >> $GITHUB_PATH
 
 # Confirm the executable is on $PATH
 which gams
