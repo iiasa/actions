28035893b13920d9d63629a81211fcd3ff0e9e31
Laura Wienpahl <57132039+LauWien@users.noreply.github.com>
2021-11-08 12:59:24 +0100

    Adjust shell.sh

--- setup-conda/script.sh
+++ setup-conda/script.sh
@@ -4,50 +4,75 @@
 pushd $GITHUB_ACTION_PATH
 
 # Create a temporary directory
-mkdir -p gams
-BASE=$(realpath gams)
+mkdir -p conda
+BASE=$(realpath conda)
 
 # GAMS source URL fragment, and path fragment for extracted files
-case $RUNNER_OS in
-  Linux)
-    CACHE_PATH="$GITHUB_ACTION_PATH/gams.exe"
-    GAMS_OS=linux
-    FRAGMENT=${GAMS_OS}_x64_64_sfx
-    ;;
-  macOS)
-    CACHE_PATH="$GITHUB_ACTION_PATH/gams.exe"
-    GAMS_OS=macosx
-    FRAGMENT=osx_x64_64_sfx
-    ;;
-  Windows)
-    CACHE_PATH="$GHA_PATH\\gams.exe"
-    GAMS_OS=windows
-    FRAGMENT=${GAMS_OS}_x64_64
-    ;;
-esac
+if [$CONDA_TYPE] == 'Anaconda'; then
+  case $RUNNER_OS in
+    Linux)
+      CACHE_PATH="$GITHUB_ACTION_PATH/conda.sh"
+      GAMS_OS=Linux
+      FRAGMENT=2021.05-${GAMS_OS}-x86_64.sh
+      ;;
+    macOS)
+      CACHE_PATH="$GITHUB_ACTION_PATH/conda.pkg"
+      GAMS_OS=MacOSX
+      FRAGMENT=2021.05-${GAMS_OS}-x86_64.pkg
+      ;;
+    Windows)
+      CACHE_PATH="$GHA_PATH\\conda.exe"
+      GAMS_OS=Windows
+      FRAGMENT=2021.05-${GAMS_OS}-x86_64.exe
+      ;;
+  esac
 
+if [$CONDA_TYPE] == 'Miniconda'; then
+  case $RUNNER_OS in
+    Linux)
+      PATH_ENDING = conda.sh
+      CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
+      GAMS_OS=Linux
+      FRAGMENT=latest-${GAMS_OS}-x86_64.sh
+      ;;
+    MacOS)
+      PATH_ENDING = conda.pkg
+      CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
+      GAMS_OS=MacOSX
+      FRAGMENT=latest-${GAMS_OS}-x86_64.pkg
+      ;;
+    Windows)
+      PATH_ENDING = conda.exe
+      CACHE_PATH="$GHA_PATH\\$PATH_ENDING"
+      GAMS_OS=Windows
+      FRAGMENT=latest-${GAMS_OS}-x86_64.exe
+      ;;
+  esac
+
+# ToDo adjust the following
 # Path fragment for extraction or install
 DEST=gams$(echo $GAMS_VERSION | cut -d. -f1-2)_$FRAGMENT
 
-# Write to special GitHub Actions environment variable to update $PATH for
-# subsequent workflow steps
-echo "$GITHUB_ACTION_PATH/$DEST" >> $GITHUB_PATH
-echo "::set-output name=cache-path::$CACHE_PATH"
 
 # Retrieve
-BASE_URL=https://d37drm4t2jghv5.cloudfront.net/distributions
-URL=$BASE_URL/$GAMS_VERSION/$GAMS_OS/$FRAGMENT.exe
+if [$CONDA_TYPE] == 'Anaconda'; then
+  BASE_URL=https://repo.anaconda.com/archive/Anaconda3-
+  URL=$BASE_URL/$CONDA_TYPE3/$GAMS_OS/$FRAGMENT
+
+if [$CONDA_TYPE] == 'Miniconda'; then
+  BASE_URL=https://repo.anaconda.com/miniconda/Miniconda3-
+  URL=$BASE_URL/$FRAGMENT
 
 # curl --time-cond only works if the named file exists
-if [ -x gams.exe ]; then
+if [ -x $PATH_ENDING ]; then
   # Don't retrieve if the remote file is older than the cached one
-  TIME_CONDITION=--remote-time --time-cond gams.exe
+  TIME_CONDITION=--remote-time --time-cond $PATH_ENDING
 fi
 
-curl --silent $URL --output gams.exe $TIME_CONDITION
+curl --silent $URL
 
-# TODO confirm checksum
 
+# ToDo adjust the following
 if [ $GAMS_OS = "windows" ]; then
   # Write a PowerShell script. Install to the same directory as *nix unzip
   cat << EOF >install-gams.ps1
