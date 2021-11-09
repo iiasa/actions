ae6f6618aa597032eb6fc8642a6ab0f1650208e3
Laura Wienpahl <57132039+LauWien@users.noreply.github.com>
2021-11-09 13:25:40 +0100

    Correct syntax

--- setup-conda/script.sh
+++ setup-conda/script.sh
@@ -9,59 +9,62 @@ BASE=$(realpath conda)
 
 # GAMS source URL fragment, and path fragment for extracted files
 if [$INSTALLER] == 'anaconda'; then
-  case $RUNNER_OS in
-    Linux)
-      CACHE_PATH="$GITHUB_ACTION_PATH/conda.sh"
-      CONDA_OS=Linux
-      FRAGMENT=$VERSION_ANACONDA-${CONDA_OS}-x86_64.sh
-      ;;
-    macOS)
-      CACHE_PATH="$GITHUB_ACTION_PATH/conda.pkg"
-      CONDA_OS=MacOSX
-      FRAGMENT=$VERSION_ANACONDA-${CONDA_OS}-x86_64.pkg
-      ;;
-    Windows)
-      CACHE_PATH="$GHA_PATH\\conda.exe"
-      CONDA_OS=Windows
-      FRAGMENT=$VERSION_ANACONDA-${CONDA_OS}-x86_64.exe
-      ;;
-  esac
-
-if [$INSTALLER] == 'miniconda'; then
+  INSTALLER_TYPE == 'Anaconda'
   case $RUNNER_OS in
     Linux)
       PATH_ENDING = conda.sh
       CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
       CONDA_OS=Linux
-      FRAGMENT=$VERSION_MINICONDA-${CONDA_OS}-x86_64.sh
+      FRAGMENT=${INSTALLER_TYPE}3-${VERSION_ANACONDA}-${CONDA_OS}-x86_64.sh
       ;;
-    MacOS)
+    macOS)
       PATH_ENDING = conda.pkg
       CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
       CONDA_OS=MacOSX
-      FRAGMENT=$VERSION_MINICONDA-${CONDA_OS}-x86_64.pkg
+      FRAGMENT=${INSTALLER_TYPE}3-${VERSION_ANACONDA}-${CONDA_OS}-x86_64.pkg
       ;;
     Windows)
       PATH_ENDING = conda.exe
       CACHE_PATH="$GHA_PATH\\$PATH_ENDING"
       CONDA_OS=Windows
-      FRAGMENT=$VERSION_MINICONDA-${CONDA_OS}-x86_64.exe
+      FRAGMENT=${INSTALLER_TYPE}3-${VERSION_ANACONDA}-${CONDA_OS}-x86_64.exe
       ;;
   esac
 
-# ToDo adjust the following
-# Path fragment for extraction or install
-DEST=gams$(echo $GAMS_VERSION | cut -d. -f1-2)_$FRAGMENT
-
-
-# Retrieve
-if [$INSTALLER] == 'Anaconda'; then
-  BASE_URL=https://repo.anaconda.com/archive/Anaconda3-
+  # Retrieve
+  BASE_URL=https://repo.anaconda.com/archive
   URL=$BASE_URL/$FRAGMENT
 
-if [$INSTALLER] == 'Miniconda'; then
-  BASE_URL=https://repo.anaconda.com/miniconda/Miniconda3-
+fi
+
+if [$INSTALLER] == 'miniconda'; then
+  INSTALLER_TYPE == 'Miniconda'
+  case $RUNNER_OS in
+    Linux)
+      PATH_ENDING = conda.sh
+      CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
+      CONDA_OS=Linux
+      FRAGMENT=${INSTALLER_TYPE}3-$VERSION_MINICONDA-${CONDA_OS}-x86_64.sh
+      ;;
+    MacOS)
+      PATH_ENDING = conda.pkg
+      CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
+      CONDA_OS=MacOSX
+      FRAGMENT=${INSTALLER_TYPE}3-${VERSION_MINICONDA}-${CONDA_OS}-x86_64.pkg
+      ;;
+    Windows)
+      PATH_ENDING = conda.exe
+      CACHE_PATH="$GHA_PATH\\$PATH_ENDING"
+      CONDA_OS=Windows
+      FRAGMENT=${INSTALLER_TYPE}3-${VERSION_MINICONDA}-${CONDA_OS}-x86_64.exe
+      ;;
+  esac
+
+  # Retrieve
+  BASE_URL=https://repo.anaconda.com/miniconda
   URL=$BASE_URL/$FRAGMENT
+
+fi
 
 # curl --time-cond only works if the named file exists
 if [ -x $PATH_ENDING ]; then
@@ -69,22 +72,22 @@ if [ -x $PATH_ENDING ]; then
   TIME_CONDITION=--remote-time --time-cond $PATH_ENDING
 fi
 
-curl --silent $URL
+curl --silent $URL --output $PATH_ENDING $TIME_CONDITION
 
-
-# ToDo adjust the following
 if [ $CONDA_OS = "windows" ]; then
-  # Write a PowerShell script. Install to the same directory as *nix unzip
-  cat << EOF >install-gams.ps1
-Start-Process "gams.exe" "/SP-", "/SILENT", "/DIR=$GHA_PATH\\$DEST", "/NORESTART" -Wait
+  # Write a PowerShell script. Install to the same directory as *nix
+  cat << EOF >install-conda.ps1
+Start-Process "$FRAGMENT" "/S", "/D=${env:GHA_PATH}\${INSTALLER_TYPE}3" -Wait
+"${env:GHA_PATH}/${INSTALLER_TYPE}3/Scripts" | Out-File -FilePath $env:GITHUB_PATH -Append
 EOF
-  cat install-gams.ps1
+  cat install-conda.ps1
 
   # Invoke the script
-  pwsh install-gams.ps1
+  pwsh install-conda.ps1
 else
   # Extract files
-  unzip -q gams.exe
+  installer -pkg $FRAGMENT -target $env:GITHUB_PATH
+  echo >> "${env:GHA_PATH}/${INSTALLER_TYPE}3/Scripts" $GITHUB_PATH
 fi
 
 # Return to the last directory
