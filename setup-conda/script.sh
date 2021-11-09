7f9d04fdf4a7e7514dbf6f4f19aa66f37f6a2ba9
Laura Wienpahl <57132039+LauWien@users.noreply.github.com>
2021-11-09 10:50:07 +0100

    Rename GAMS_OS to CONDA_OS

--- setup-conda/script.sh
+++ setup-conda/script.sh
@@ -12,18 +12,18 @@ if [$INSTALLER] == 'anaconda'; then
   case $RUNNER_OS in
     Linux)
       CACHE_PATH="$GITHUB_ACTION_PATH/conda.sh"
-      GAMS_OS=Linux
-      FRAGMENT=$VERSION_ANACONDA-${GAMS_OS}-x86_64.sh
+      CONDA_OS=Linux
+      FRAGMENT=$VERSION_ANACONDA-${CONDA_OS}-x86_64.sh
       ;;
     macOS)
       CACHE_PATH="$GITHUB_ACTION_PATH/conda.pkg"
-      GAMS_OS=MacOSX
-      FRAGMENT=$VERSION_ANACONDA-${GAMS_OS}-x86_64.pkg
+      CONDA_OS=MacOSX
+      FRAGMENT=$VERSION_ANACONDA-${CONDA_OS}-x86_64.pkg
       ;;
     Windows)
       CACHE_PATH="$GHA_PATH\\conda.exe"
-      GAMS_OS=Windows
-      FRAGMENT=$VERSION_ANACONDA-${GAMS_OS}-x86_64.exe
+      CONDA_OS=Windows
+      FRAGMENT=$VERSION_ANACONDA-${CONDA_OS}-x86_64.exe
       ;;
   esac
 
@@ -32,20 +32,20 @@ if [$INSTALLER] == 'miniconda'; then
     Linux)
       PATH_ENDING = conda.sh
       CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
-      GAMS_OS=Linux
-      FRAGMENT=$VERSION_MINICONDA-${GAMS_OS}-x86_64.sh
+      CONDA_OS=Linux
+      FRAGMENT=$VERSION_MINICONDA-${CONDA_OS}-x86_64.sh
       ;;
     MacOS)
       PATH_ENDING = conda.pkg
       CACHE_PATH="$GITHUB_ACTION_PATH/$PATH_ENDING"
-      GAMS_OS=MacOSX
-      FRAGMENT=$VERSION_MINICONDA-${GAMS_OS}-x86_64.pkg
+      CONDA_OS=MacOSX
+      FRAGMENT=$VERSION_MINICONDA-${CONDA_OS}-x86_64.pkg
       ;;
     Windows)
       PATH_ENDING = conda.exe
       CACHE_PATH="$GHA_PATH\\$PATH_ENDING"
-      GAMS_OS=Windows
-      FRAGMENT=$VERSION_MINICONDA-${GAMS_OS}-x86_64.exe
+      CONDA_OS=Windows
+      FRAGMENT=$VERSION_MINICONDA-${CONDA_OS}-x86_64.exe
       ;;
   esac
 
@@ -73,7 +73,7 @@ curl --silent $URL
 
 
 # ToDo adjust the following
-if [ $GAMS_OS = "windows" ]; then
+if [ $CONDA_OS = "windows" ]; then
   # Write a PowerShell script. Install to the same directory as *nix unzip
   cat << EOF >install-gams.ps1
 Start-Process "gams.exe" "/SP-", "/SILENT", "/DIR=$GHA_PATH\\$DEST", "/NORESTART" -Wait
