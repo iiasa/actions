21fd166bb21a193c0856f6595060670422728d60
Laura Wienpahl <laura@wienpahl.de>
2021-11-12 13:16:29 +0100

    Correct typos and set ls command

--- setup-conda/script.sh
+++ setup-conda/script.sh
@@ -50,6 +50,8 @@ DEST="${INSTALLER_TYPE}3"
 echo "$GITHUB_ACTION_PATH/$DEST/Scripts" >> $GITHUB_PATH
 echo "::set-output name=cache-path::$CACHE_PATH"
 
+ls -l
+
 # Retrieve
 BASE_URL=https://repo.anaconda.com
 URL=$BASE_URL/$URL_FRAGMENT/$INSTALL_FILE
@@ -71,7 +73,7 @@ case $RUNNER_OS in
     # -b run install in batch mode (without manual intervention):
     #  Accepts the Licence Agreement and allows Anaconda to be added to the `PATH`.
     # -p PREFIX install prefix, defaults to $PREFIX, must not contain spaces. Default PREFIX=$HOME/anaconda3
-    conda.$EXT -b -p "$GITHUB_ACTION_PATH/$DEST"
+    bash "conda.$EXT" -b -p "$GITHUB_ACTION_PATH/$DEST"
     # Load the  `PATH` environment variable in current terminal session
     source ~/.bashrc
     ;;
@@ -82,7 +84,7 @@ case $RUNNER_OS in
   Windows)
     # Write a PowerShell script. Install to the same directory as *nix
     cat << EOF >install-conda.ps1
-Start-Process conda.$EXT "/SP-", "/SILENT", "/DIR=$GHA_PATH\\$DEST", "/NORESTART" -Wait
+Start-Process "conda.$EXT" "/SP-", "/SILENT", "/DIR=$GHA_PATH\\$DEST", "/NORESTART" -Wait
 EOF
     cat install-conda.ps1
 
