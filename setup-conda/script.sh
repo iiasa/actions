aef391a3d7dc3f9144ebcf4d648cc290546a078d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-11-12 10:47:03 +0100

    Simplify path handling in setup-conda

--- setup-conda/script.sh
+++ setup-conda/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Change to the direction containing the action's code
+# Change to the directory containing the action's code
 pushd $GITHUB_ACTION_PATH
 
-# Create a temporary directory
-mkdir -p conda
-BASE=$(realpath conda)
+# # Create a temporary directory
+# mkdir -p conda
+# BASE=$(realpath conda)
 
 anaconda="anaconda"
 miniconda="miniconda"
@@ -43,17 +43,19 @@ esac
 INSTALL_FILE=${INSTALLER_TYPE}3-${VERSION}-${CONDA_OS}-x86_64.${EXT}
 
 # Path fragment for extraction or install
-DEST="conda/${INSTALLER_TYPE}3-${VERSION}-${CONDA_OS}-x86_64"
+DEST="${INSTALLER_TYPE}3"
 
 # Write to special GitHub Actions environment variable to update $PATH for
 # subsequent workflow steps
-echo "$GITHUB_ACTION_PATH/$DEST" >> $GITHUB_PATH
+echo "$GITHUB_ACTION_PATH/$DEST/Scripts" >> $GITHUB_PATH
 echo "::set-output name=cache-path::$CACHE_PATH"
-
 
 # Retrieve
 BASE_URL=https://repo.anaconda.com
 URL=$BASE_URL/$URL_FRAGMENT/$INSTALL_FILE
+
+# Debug
+echo "$URL"
 
 # curl --time-cond only works if the named file exists
 if [ -x "conda.${EXT}" ]; then
@@ -67,8 +69,7 @@ curl --silent $URL --output "conda.${EXT}" $TIME_CONDITION
 if [ $CONDA_OS == "windows-latest" ]; then
   # Write a PowerShell script. Install to the same directory as *nix
   cat << EOF >install-conda.ps1
-Start-Process "$INSTALL_FILE" "/SP-", "/SILENT", "/DIR=${env:GHA_PATH}\${INSTALLER_TYPE}3", "/NORESTART" -Wait
-"${env:GHA_PATH}/${INSTALLER_TYPE}3/Scripts" | Out-File -FilePath $env:GITHUB_PATH -Append
+Start-Process "$INSTALL_FILE" "/SP-", "/SILENT", "/DIR=$GHA_PATH\\$DEST", "/NORESTART" -Wait
 EOF
   cat install-conda.ps1
 
@@ -78,8 +79,7 @@ fi
 
 if [ $CONDA_OS == "macos-latest" ]; then
   # Extract files
-  installer -pkg $INSTALL_FILE -target "${env:GHA_PATH}/${INSTALLER_TYPE}3"
-  echo >> "${env:GHA_PATH}/${INSTALLER_TYPE}3/Scripts" $GITHUB_PATH
+  installer -pkg $INSTALL_FILE -target "$GITHUB_ACTION_PATH/$DEST"
 fi
 
 if [ $CONDA_OS == "ubuntu-latest" ]; then
@@ -87,7 +87,7 @@ if [ $CONDA_OS == "ubuntu-latest" ]; then
   # -b run install in batch mode (without manual intervention):
   #  Accepts the Licence Agreement and allows Anaconda to be added to the `PATH`.
   # -p PREFIX install prefix, defaults to $PREFIX, must not contain spaces. Default PREFIX=$HOME/anaconda3
-  bash $INSTALL_FILE -b -p "${env:GHA_PATH}/${INSTALLER_TYPE}3"
+  bash $INSTALL_FILE -b -p "$GITHUB_ACTION_PATH/$DEST"
   # Load the  `PATH` environment variable in current terminal session
   source ~/.bashrc
 fi
